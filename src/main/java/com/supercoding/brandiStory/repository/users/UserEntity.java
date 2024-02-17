6c95b9c86085856674a4d265f2ce7bcb821d11da
JiseopSong <wltkqdl@gmail.com>
2024-02-18 00:54:30 +0900

    유저 로그인 필수기능 <참고> 2번째 기능 추가
    2. 비밀번호 생성 시 영문자, 숫자의 조합으로 8자 이상 20자 이하
    
    etc. 이메일 형식에 맞게 validation 추가

--- src/main/java/com/supercoding/brandiStory/repository/users/UserEntity.java
+++ src/main/java/com/supercoding/brandiStory/repository/users/UserEntity.java
@@ -2,6 +2,7 @@ package com.supercoding.brandiStory.repository.users;
 
 import com.supercoding.brandiStory.repository.users.enums.SexType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,10 +18,12 @@ public class UserEntity {
     @Id @Column @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false, unique = true)
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
     @Column(length = 20)
     private String username;
     @Column(length = 20)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$", message = "비밀번호는 영문자와 숫자의 조합으로 8자 이상 20자 이하여야 합니다.")
     private String password;
     @Column(name = "phone", length = 20)
     private String phoneNumber;
