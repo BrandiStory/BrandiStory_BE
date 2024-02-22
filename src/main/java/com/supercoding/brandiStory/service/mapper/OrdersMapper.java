2a8a0b33a36e0c8421f4d902dac57f7ff57f14dd
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-02-22 22:07:27 +0900

    Add files via upload

---
+++ src/main/java/com/supercoding/brandiStory/service/mapper/OrdersMapper.java
@@ -0,0 +1,26 @@
+package com.supercoding.brandiStory.service.mapper;
+
+import com.supercoding.brandiStory.repository.carts.CartItemJpaRepository;
+import com.supercoding.brandiStory.repository.entity.OrdersEntity;
+import com.supercoding.brandiStory.web.dto.OrderListReqDto;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface OrdersMapper {
+
+    OrdersMapper INSTANCE = Mappers.getMapper(OrdersMapper.class);
+
+    @Mapping(target = "cartItemsId", ignore = true)
+    @Mapping(source = "customerName", target = "customerName")
+    @Mapping(source = "customerPhone", target = "customerPhone")
+    @Mapping(source = "customerEmail", target = "customerEmail")
+    @Mapping(source = "deliveryAddress", target = "deliveryAddress")
+    OrdersEntity orderDtoToEntity(OrderListReqDto orderDTO);
+    @AfterMapping
+    default void mapCartItemsId(@MappingTarget OrdersEntity order, OrderListReqDto orderDTO, @Context CartItemJpaRepository cartItemRepository) {
+        if (orderDTO.getCartItemsId() != null) {
+            order.setCartItemsId(cartItemRepository.findById(orderDTO.getCartItemsId()).orElse(null));
+        }
+    }
+}
