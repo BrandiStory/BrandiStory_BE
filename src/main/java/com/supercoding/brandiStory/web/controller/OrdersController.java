21efc1cb3d868316449581688f903c6c0adaa674
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-02-23 20:04:36 +0900

    Update OrdersController.java

--- src/main/java/com/supercoding/brandiStory/web/controller/OrdersController.java
+++ src/main/java/com/supercoding/brandiStory/web/controller/OrdersController.java
@@ -3,17 +3,16 @@ package com.supercoding.brandiStory.web.controller;
 import com.supercoding.brandiStory.repository.entity.OrdersEntity;
 import com.supercoding.brandiStory.service.OrdersService;
 import com.supercoding.brandiStory.web.dto.OrderListReqDto;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.Map;
+
 @RestController
-@Slf4j
-public class OrdersController implements ApiController {
+@RequestMapping("/orders")
+public class OrdersController {
 
     private final OrdersService ordersService;
 
@@ -21,24 +20,30 @@ public class OrdersController implements ApiController {
     public OrdersController(OrdersService ordersService) {
         this.ordersService = ordersService;
     }
-    @Operation(summary = "주문하기")
-    @PostMapping("/orders")
+
+    @PostMapping("")
     public ResponseEntity<?> createOrder(@RequestBody OrderListReqDto orderDTO) {
+        System.out.println("orderDTO >>> "+ orderDTO);
         try {
             OrdersEntity newOrder = ordersService.createOrder(orderDTO);
+            System.out.println("sdfsdfs" + newOrder);
             return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
         } catch (RuntimeException e) {
+            System.out.println("sdfsdfs" + e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-    @Operation(summary = "주문조회")
-    @GetMapping("/orders/{id}")
+
+    @GetMapping("/{id}")
     public ResponseEntity<?> getOrderById(@PathVariable Integer id) {
         try {
             OrdersEntity order = ordersService.getOrderById(id);
+
             return ResponseEntity.ok(order);
         } catch (RuntimeException e) {
+
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+
         }
     }
 }
