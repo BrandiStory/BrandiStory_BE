f0aabba1a0a46229c9b53aae327cc08edd714fd5
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-02-23 20:03:01 +0900

    Update OrdersEntity.java

--- src/main/java/com/supercoding/brandiStory/repository/entity/OrdersEntity.java
+++ src/main/java/com/supercoding/brandiStory/repository/entity/OrdersEntity.java
@@ -23,27 +23,27 @@ public class OrdersEntity {
     @JoinColumn(name = "users_id", nullable = false)
     private UserEntity userEntity;
 
-    @Column(name = "product_name", nullable = false)
+    @Column(nullable = false)
     private String productName;
 
     @Column(name = "quantity")
     private Integer quantity;
 
-    @Column(name = "total_price")
-    private Integer totalPrice;
+//    @Column(name = "total_price")
+//    private Integer totalPrice;
 
-    @Column(name = "customer_name", nullable = false)
+    @Column(nullable = false)
     private String customerName;
 
-    @Column(name = "customer_phone", nullable = false)
+    @Column(nullable = false)
     private String customerPhone;
 
-    @Column(name = "customer_email")
+    @Column(nullable = false)
     private String customerEmail;
 
-    @Column(name = "delivery_address", nullable = false)
+    @Column(nullable = false)
     private String deliveryAddress;
 
-    @Column(name = "order_date", nullable = false)
+    @Column(nullable = false)
     private LocalDateTime orderDate;
 }
