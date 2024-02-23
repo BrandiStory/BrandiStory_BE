c265727023fcd0cc47259c635b91378ce195b437
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-02-23 20:02:06 +0900

    Add files via upload

---
+++ src/main/java/com/supercoding/brandiStory/repository/orders/OrdersJpaRepository.java
@@ -0,0 +1,10 @@
+package com.supercoding.brandiStory.repository.orders;
+
+import com.supercoding.brandiStory.repository.entity.OrdersEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersJpaRepository extends JpaRepository<OrdersEntity, Integer> {
+
+}
